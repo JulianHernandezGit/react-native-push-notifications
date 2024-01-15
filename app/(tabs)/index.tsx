7597bc435d77af7f6157d722f516d520008e8690
Julian Hernandez <66444617+JulianHernandezGit@users.noreply.github.com>
2024-01-15 08:41:31 -0300

    Update Changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,63 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { View } from '../../components/Themed';
+import { useEffect, useRef } from 'react';
+import * as Notifications from 'expo-notifications';
 
 export default function TabOneScreen() {
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    Notifications.requestPermissionsAsync({
+      ios: {
+        allowAlert: true,
+        allowBadge: true,
+        allowSound: true,
+        allowAnnouncements: true,
+      },
+    }).then((status) => {
+      console.log('PERMS: ', status);
+    })
+
+    // // This listener is fired whenever a notification is received while the app is foregrounded
+    // notificationListener.current = Notifications.addNotificationReceivedListener(async(notification) => {
+    //   console.log('NOTIF: ', notification);
+    //   const count = await Notifications.getBadgeCountAsync();
+    //   console.log('COUNT: ', count);
+    //   await Notifications.setBadgeCountAsync(count + 1);
+    // });
+
+    // //Tap on notification to open app
+    // responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+    //   console.log('RESPONSE: ', response);
+    //   Notifications.dismissAllNotificationsAsync();
+    //   Notifications.setBadgeCountAsync(0);
+    // });
+
+    // Clean up the listener on unmount
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current!);
+      Notifications.removeNotificationSubscription(responseListener.current!);
+    }
+  }, []);
+
+  const scheduleNotification = async () => {
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail!",
+        body: 'My notification body',
+        data: { data: 'Secret message' },
+      },
+      trigger: {
+        seconds: 4,
+      },
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button onPress={scheduleNotification} title="Schedule Notification" />
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,55 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
+import { SplashScreen, Stack, router } from 'expo-router';
 import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Device from 'expo-device';
+
+// Register the push notification handler
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+})
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      console.log('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync({
+      projectId: Constants.expoConfig?.extra?.eas.projectId,
+    })).data;
+    //token = (await Notifications.getDevicePushTokenAsync()).data; > Traditional method
+    console.log('TOKEN: ', token);
+  } else {
+    console.log('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -24,6 +70,13 @@ export default function RootLayout() {
     ...FontAwesome.font,
   });
 
+  // Register for push notifications
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => {
+      console.log('TOKEN: ', token);
+    });
+  }, []);
+
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
   useEffect(() => {
     if (error) throw error;
@@ -42,8 +95,39 @@ export default function RootLayout() {
   return <RootLayoutNav />;
 }
 
+function useNotificationObserver() {
+  useEffect(() => {
+    let isMounted = true;
+
+    function redirect(notification: Notifications.Notification) {
+      const url = notification.request.content.data?.url;
+      if (url) {
+        router.push(url);
+      }
+    }
+
+    Notifications.getLastNotificationResponseAsync()
+      .then(response => {
+        if (!isMounted || !response?.notification) {
+          return;
+        }
+        redirect(response?.notification);
+      });
+
+    const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+      redirect(response.notification);
+    });
+
+    return () => {
+      isMounted = false;
+      subscription.remove();
+    };
+  }, []);
+}
+
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
+  useNotificationObserver();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
